60ec55f6f2d6ae3865f75bcd44893c989e766f18
Emilien Kenler <hello@emilienkenler.com>
2013-10-22 11:08:34 +0200

    Generate Grid with AngularJS
    Signed-off-by: Emilien Kenler <hello@emilienkenler.com>

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 $(document).ready(function() {
-  for(var j = 0; j < 16; j++) {
-    var row = $('<div class="row row'+j+'"></div>');
-    for(var i = 0; i < 24; i++) {
-      var cell = $('<div class="cell col'+i+'"></div>');
-      cell.appendTo(row);
-    }
-    $('.grid').append(row);
-  }
-  $('.cell').on('click', function() {
-    if($(this).hasClass('active'))
-      $(this).removeClass('active');
-    else
-      $(this).addClass('active');
-  });
+  // Background
   var updateBg = function() {
     var bgColor = '';
     bgColor += (Math.ceil(Math.random() * 255)).toString(16);
     bgColor += (Math.ceil(Math.random() * 255)).toString(16);
     bgColor += (Math.ceil(Math.random() * 255)).toString(16);
     $('body').css('background-color','#'+bgColor);
-    setTimeout(updateBg,1000);
+    setTimeout(updateBg,500);
   };
-  setTimeout(updateBg,1000);
+  setTimeout(updateBg,500);
 });
 
+function initGrid(grid, height, width) {
+  for(var i = 0; i < height; i++) {
+    var row = [];
+    for(var j = 0; j < width; j++) {
+      row.push({ row: i, col: j, active: false });
+    }
+    grid.push(row);
+  }
+}
+
+function GridCtrl($scope) {
+  $scope.grid = [];
+  initGrid($scope.grid, 8, 8);
+
+  $scope.onCellClick = function(row,col) {
+    $scope.grid[row][col].active = !($scope.grid[row][col].active);
+  }
+}
