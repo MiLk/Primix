aa10fc86d7a58fa7414b2b9b74d926ec0e6bd864
Guillaume George <guillaume.george@etu.utc.fr>
2013-10-29 11:06:09 -0400

    fill grid with the input number
    Signed-off-by: Guillaume George <guillaume.george@etu.utc.fr>

--- js/main.js
+++ js/main.js
@@ -15,14 +15,16 @@ function GlobalCtrl($scope) {
   $scope.isGridVisible = false;
   $scope.primeArrays  = [];
   $scope.primeArraysIndexes = [];
+  $scope.gridIndexArrays = [];
 
   $scope.toggleGrid = function() {
     $scope.isGridVisible = !($scope.isGridVisible);
-  }
+  };
   
   $scope.onInputChange = function() {
     $scope.decompose(8);
-  }
+    $scope.fillGrid(8);
+  };
   
   /*
    * decompose number in n subnumbers
@@ -49,7 +51,25 @@ function GlobalCtrl($scope) {
     
     console.log('primeArrays = ' + $scope.primeArrays.join(' * '));
     console.log('Indexes     = ' + $scope.primeArraysIndexes.join(' * '));
-  }
+  };
+  
+  $scope.fillGrid = function(height) {
+    $scope.gridIndexArrays = [];
+    for(var i = 0; i < $scope.primeArraysIndexes.length; ++i) {
+      $scope.gridIndexArrays[i] = [];
+      for(var j = 0; j < $scope.primeArraysIndexes[i].length; ++j) {
+        // Avoid duplicated values
+        if($scope.primeArraysIndexes[i][j] != -1
+        && $scope.gridIndexArrays[i].indexOf($scope.primeArraysIndexes[i][j] % height) == -1) {
+          $scope.gridIndexArrays[i].push($scope.primeArraysIndexes[i][j] % height);
+        }
+      }
+    }
+    
+    console.log('%%Indexes%% = ' + $scope.gridIndexArrays.join(' * '));
+    
+    $scope.$broadcast('UPDATE_GRID');
+  };
   
 }
 
@@ -63,12 +83,51 @@ function initGrid(grid, height, width) {
   }
 }
 
+function initIndexes(array, height, width) {
+  for(var i = 0; i < height; ++i) {
+    array[i] = [];
+    for(var j = 0; j < width; ++j) {
+      array[i][(j-i)%height] = j;
+    }
+  }
+}
+
 function GridCtrl($scope) {
   $scope.grid = [];
+  $scope.gridIndexes = [];
   initGrid($scope.grid, 8, 8);
+  initIndexes($scope.gridIndexes, 8, 8);
 
   $scope.onCellClick = function(row,col) {
     $scope.grid[row][col].active = !($scope.grid[row][col].active);
-  }
+  };
+  
+  $scope.reset = function() {
+    for(var i = 0; i < $scope.grid.length; ++i) {
+      for(var j = 0; j < $scope.grid[i].length; ++j) {
+        this.disable(i, j);
+      }
+    }
+  };
+  
+  $scope.enable = function(x, y) {
+    $scope.grid[y][x].active = true;
+  };
+  
+  $scope.disable = function(x, y) {
+    $scope.grid[y][x].active = false;
+  };
+  
+  $scope.$on('UPDATE_GRID', function() {
+    var arr = $scope.$parent.gridIndexArrays;
+    
+    $scope.reset();
+    for(var i = 0; i < arr.length; ++i) {
+      for(var j = 0; j < arr[i].length; ++j) {
+        console.log('enable ' + i + ',' + $scope.gridIndexes[i][arr[i][j]]);
+        $scope.enable(i, $scope.gridIndexes[i][arr[i][j]]);
+      }
+    }
+  });
 }
 
